5feebff3de70d8e640a88cad12cb52fb75cf2043
jlange <jlange54@gmx.de>
2023-10-26 17:12:37 +0200

    404GP-2 Add GetAll / GetById to Feature Ctrler

--- 404_game_portal.backend/Controllers/FeatureController.cs
+++ 404_game_portal.backend/Controllers/FeatureController.cs
@@ -1,4 +1,5 @@
-﻿using _404_game_portal.backend.Repositories;
+﻿using _404_game_portal.backend.Entities;
+using _404_game_portal.backend.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _404_game_portal.backend.Controllers;
@@ -12,5 +13,23 @@ public class FeatureController : ControllerBase
     public FeatureController(IFeatureRepository featureRepository)
     {
         _featureRepository = featureRepository;
+    }
+
+    [HttpGet]
+    public List<Feature> GetAll()
+    {
+        return _featureRepository.GetAll();
+    }
+
+    [HttpGet("{id:guid}")]
+    public Feature GetById(Guid id)
+    {
+        return _featureRepository.GetById(id);
+    }
+
+    [HttpPost]
+    public Feature Create()
+    {
+        
     }
 }
