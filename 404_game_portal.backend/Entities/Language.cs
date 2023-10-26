721c05eccda1b9e4d41be95ca09394e8bf58047d
Ian Risch <ian.risch@gmail.com>
2023-10-26 17:21:48 +0200

    404GPG-2 Refactor Language VM to have Id not nulled

--- 404_game_portal.backend/Entities/Language.cs
+++ 404_game_portal.backend/Entities/Language.cs
@@ -4,16 +4,14 @@ namespace _404_game_portal.backend.Entities;
 
 public class Language
 {
-    public Guid Id  { get; set; }
+    public Guid Id { get; set; }
 
     public List<Game> Games { get; set; }
     public string LanguageName { get; set; }
 
     public Language(LanguageViewModel languageViewModel)
     {
-        if (languageViewModel.Id is not null)
-            Id = (Guid)languageViewModel.Id;
-
+        Id = languageViewModel.Id;
         LanguageName = languageViewModel.LanguageName;
     }
 
--- 404_game_portal.backend/ViewModels/LanguageViewModel.cs
+++ 404_game_portal.backend/ViewModels/LanguageViewModel.cs
@@ -2,7 +2,7 @@ namespace _404_game_portal.backend.ViewModels;
 
 public class LanguageViewModel
 {
-    public Guid? Id  { get; set; }
+    public Guid Id  { get; set; }
 
     public List<GameViewModel> Games { get; set; }
 
