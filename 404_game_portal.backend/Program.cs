1fc82182f798da99379be6d8fb0eac1b5017ddd3
jlange <jlange54@gmx.de>
2023-10-26 14:25:50 +0200

    404GP-2 Refactor Services into Startup

--- 404_game_portal.backend/Program.cs
+++ 404_game_portal.backend/Program.cs
@@ -2,22 +2,8 @@ using _404_game_portal.backend;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<Context>(dbContextOptions => dbContextOptions
-    .LogTo(Console.WriteLine, LogLevel.Information)
-    .EnableSensitiveDataLogging()
-    .EnableDetailedErrors());
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "MyAllowSpecificOrigins",
-        corsPolicyBuilder => { corsPolicyBuilder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();  });
-});
-
+var startup = new Startup(builder.Services);
+startup.Execute();
 
 var app = builder.Build();
 
---
+++ 404_game_portal.backend/Startup.cs
@@ -0,0 +1,42 @@
+﻿namespace _404_game_portal.backend;
+
+public class Startup
+{
+    private IServiceCollection _services { get; set; }
+
+    public Startup(IServiceCollection services)
+    {
+        _services = services;
+    }
+
+    public void Execute()
+    {
+        AddServices();
+    }
+    
+    private void AddServices()
+    {
+        _services.AddControllers();
+        _services.AddEndpointsApiExplorer();
+        _services.AddSwaggerGen();
+        AddCustomDbContext();
+        AddCustomCors();
+    }
+
+    private void AddCustomDbContext()
+    {
+        _services.AddDbContext<Context>(dbContextOptions => dbContextOptions
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors());
+    }
+
+    private void AddCustomCors()
+    {
+        _services.AddCors(options =>
+        {
+            options.AddPolicy(name: "MyAllowSpecificOrigins",
+                corsPolicyBuilder => { corsPolicyBuilder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();  });
+        });  
+    }
+}
