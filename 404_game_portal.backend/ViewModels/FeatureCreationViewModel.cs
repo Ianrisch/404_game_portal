dd5b0145bfc4c266a50adab7d65befbfeeb07bd0
jlange <jlange54@gmx.de>
2023-10-26 17:28:53 +0200

    404GP-2 Add FeatureCreationVM

---
+++ 404_game_portal.backend/ViewModels/FeatureCreationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using _404_game_portal.backend.Entities;
+
+namespace _404_game_portal.backend.ViewModels;
+
+public class FeatureCreationViewModel
+{
+    public List<Guid> Games { get; set; }
+    
+    public string FeatureName { get; set; }
+    public string FeatureDescription { get; set; }
+}
