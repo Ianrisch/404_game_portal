4323c1f2ea1ccede0b9f86702f9e86c41d0c727b
jlange <jlange54@gmx.de>
2023-10-26 18:08:42 +0200

    404GP-2 Add Platform Ctrler

---
+++ 404_game_portal.backend/Controllers/PlatformController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _404_game_portal.backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PlatformController : ControllerBase
+{
+    private readonly
+}
