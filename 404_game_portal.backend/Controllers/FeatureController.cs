00449a497deed4053b3708b85b8172a528965b8e
jlange <jlange54@gmx.de>
2023-10-26 17:03:17 +0200

    404GP-2 Add Feature Ctrler

---
+++ 404_game_portal.backend/Controllers/FeatureController.cs
@@ -0,0 +1,16 @@
+﻿using _404_game_portal.backend.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _404_game_portal.backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FeatureController : ControllerBase
+{
+    private readonly IFeatureRepository _featureRepository;
+
+    public FeatureController(IFeatureRepository featureRepository)
+    {
+        _featureRepository = featureRepository;
+    }
+}
