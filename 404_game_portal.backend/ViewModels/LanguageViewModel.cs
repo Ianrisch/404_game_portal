cbe795bba1cf70d821c9235c492135c1378fed2d
Ian Risch <ian.risch@gmail.com>
2023-10-26 16:52:39 +0200

    404GPG-2 Add Games to Language ViewModel

--- 404_game_portal.backend/ViewModels/LanguageViewModel.cs
+++ 404_game_portal.backend/ViewModels/LanguageViewModel.cs
@@ -3,5 +3,8 @@ namespace _404_game_portal.backend.ViewModels;
 public class LanguageViewModel
 {
     public Guid? Id  { get; set; }
+
+    public List<GameViewModel> Games { get; set; }
+
     public string LanguageName { get; set; }
 }
