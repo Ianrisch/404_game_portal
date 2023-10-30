7c5f0e4b32d0b6efb318d180567f90e30e3bcd04
jlange <jlange54@gmx.de>
2023-10-30 09:16:27 +0100

    404GP-2 Add PlatformCreationVM

---
+++ 404_game_portal.backend/ViewModels/PlatformCreationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using _404_game_portal.backend.Entities;
+
+namespace _404_game_portal.backend.ViewModels;
+
+public class PlatformCreationViewModel
+{
+    public Guid Id  { get; set; }
+
+    public List<Guid> Games { get; set; }
+    public List<PriceOnPlatform> Prices { get; set; }
+    public string PlatformName { get; set; }
+    public string PlatformVersion { get; set; }
+    public string PlatformType { get; set; }
+}
