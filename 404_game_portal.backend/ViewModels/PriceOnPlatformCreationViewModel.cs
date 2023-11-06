1499520588a127df48edd5e4de471096729923a7
Ian Risch <ian.risch@brickmakers.de>
2023-11-06 08:58:43 +0100

    404GP-2 Create PlatformViewModel

--- 404_game_portal.backend/ViewModels/PriceOnPlatformCreationViewModel.cs
+++ 404_game_portal.backend/ViewModels/PriceOnPlatformCreationViewModel.cs
@@ -1,10 +1,9 @@
-using _404_game_portal.backend.Entities;
 
 namespace _404_game_portal.backend.ViewModels;
 
 public class PriceOnPlatformCreationViewModel
 {
-    public int Price { get; set; }
-    public Game Game { get; set; }
-    public Platform Platform { get; set; }
+    public double Price { get; set; }
+    public Guid Game { get; set; }
+    public Guid Platform { get; set; }
 }
