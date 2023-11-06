3321da02fdc426c22302ee67e2b3d622e19ba93b
Ian Risch <ian.risch@brickmakers.de>
2023-11-06 09:01:38 +0100

    404GP-2 Change Price To Double

--- 404_game_portal.backend/Entities/PriceOnPlatform.cs
+++ 404_game_portal.backend/Entities/PriceOnPlatform.cs
@@ -1,23 +1,10 @@
-using _404_game_portal.backend.ViewModels;
-
 namespace _404_game_portal.backend.Entities;
 
 public class PriceOnPlatform
 {
-    public PriceOnPlatform(PriceOnPlatformCreationViewModel creationViewModel)
-    {
-        Price = creationViewModel.Price;
-        Game = creationViewModel.Game;
-        Platform = creationViewModel.Platform;
-    }
-
-    public PriceOnPlatform()
-    {
-    }
-
     public Guid Id { get; set; }
 
-    public int Price { get; set; }
+    public double Price { get; set; }
     public Game Game { get; set; }
     public Guid PlatformId { get; set; }
     public Platform Platform { get; set; }
