7a77dbc6857649a9948033d0754fa2916d9e85f3
Ian Risch <ian.risch@brickmakers.de>
2023-11-15 11:49:50 +0100

    Proxy dev Server

--- 404_game_portal.frontend/vite.config.ts
+++ 404_game_portal.frontend/vite.config.ts
@@ -7,35 +7,81 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    outDir: '../404_game_portal.backend/wwwroot'
-  },
-  plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-    }),
-  ],
-  define: { 'process.env': {} },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ],
-  },
-  server: {
-    port: 3000,
-  },
-})
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [
+        vue({
+          template: { transformAssetUrls }
+        }),
+        // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+        vuetify({
+          autoImport: true,
+        }),
+      ],
+      define: { 'process.env': {} },
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+
+        extensions: [
+          '.js',
+          '.json',
+          '.jsx',
+          '.mjs',
+          '.ts',
+          '.tsx',
+          '.vue',
+        ],
+      },
+      server: {
+        port: 3000,
+        proxy: {
+          '/api': {
+            target: 'https://localhost:7016',
+            changeOrigin: true,
+            secure: false,
+          },
+        },
+      },
+    }
+  } else {
+    // command === 'build'
+    return {
+      build: {
+        outDir: '../404_game_portal.backend/wwwroot'
+      },
+      plugins: [
+        vue({
+          template: { transformAssetUrls }
+        }),
+        // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+        vuetify({
+          autoImport: true,
+        }),
+      ],
+        define: { 'process.env': {} },
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+
+        extensions: [
+          '.js',
+          '.json',
+          '.jsx',
+          '.mjs',
+          '.ts',
+          '.tsx',
+          '.vue',
+        ],
+      },
+      server: {
+        port: 3000,
+      },
+    }
+
+  }
+}
+)
