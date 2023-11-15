7596b940b7838daab6eddc4014c18e1476a9865f
Ian Risch <ian.risch@brickmakers.de>
2023-11-15 11:52:19 +0100

    Add Game Card to HomeView

---
+++ 404_game_portal.frontend/src/components/GameCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Game } from '@/api/game';
+import { USK } from '@/api/game';
+defineProps<{
+  game: Game;
+}>();
+</script>
+
+<template>
+  <v-card>
+    <v-card-title>
+      {{ game.name }}
+    </v-card-title>
+    <v-card-text>
+      {{ game.description }}
+      {{ USK[game.usk] }}
+    </v-card-text>
+    <VCardActions>
+      <VBtn @click="$router.push(`/game/${game.id}`)">Mehr</VBtn>
+    </VCardActions>
+  </v-card>
+</template>
+
+<style scoped></style>
--- 404_game_portal.frontend/src/views/Home.vue
+++ 404_game_portal.frontend/src/views/Home.vue
@@ -1,7 +1,18 @@
 <template>
   <HelloWorld />
+  <VList :loading="loading">
+    <template v-if="result">
+      <GameCard v-for="game in result" :key="game.id" :game="game" />
+    </template>
+  </VList>
 </template>
 
 <script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import usePromise from '@/composables/usePromise';
+import api from '@/api';
+import GameCard from '@/components/GameCard.vue';
+
+const { loading, result, createPromise } = usePromise(api.fetchGames);
+createPromise();
 </script>
