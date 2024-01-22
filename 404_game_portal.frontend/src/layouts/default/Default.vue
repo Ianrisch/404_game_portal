ea6d3604d354ab94e9923b4ca192e4591a3e9715
jlange <jlange54@gmx.de>
2024-01-22 09:40:09 +0100

    404GP-3 Implement Theme switcher

--- 404_game_portal.frontend/src/layouts/default/Default.vue
+++ 404_game_portal.frontend/src/layouts/default/Default.vue
@@ -1,9 +1,28 @@
 <template>
   <v-app>
-    <default-view />
+    <v-layout>
+      <v-app-bar>
+        <template #append>
+          <v-switch
+            v-model:model-value="isDarkMode"
+            :label="isDarkMode ? 'Change to Lightmode' : 'Change to Darkmode'"
+          />
+        </template>
+      </v-app-bar>
+
+      <v-main>
+        <router-view />
+      </v-main>
+    </v-layout>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  import DefaultView from './View.vue'
+import { ref, watch } from 'vue';
+const theme = useTheme();
+import { useTheme } from 'vuetify';
+const isDarkMode = ref<boolean>(theme.global.current.value.dark);
+watch(isDarkMode, (isDark) => {
+  theme.global.name.value = isDark ? 'dark' : 'light';
+});
 </script>
