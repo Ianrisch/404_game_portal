27db08e86ed60c57d5d6824d05489dfdba31dd28
Ian Risch <ian.risch@gmail.com>
2024-02-05 10:23:44 +0100

    404-Gp-3 Fix Api Endpoints

--- 404_game_portal.backend/Controllers/AuthController.cs
+++ 404_game_portal.backend/Controllers/AuthController.cs
@@ -1,7 +1,9 @@
 using System.Security.Claims;
 using _404_game_portal.backend.Services;
+using _404_game_portal.backend.ViewModels;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _404_game_portal.backend.Controllers;
@@ -11,17 +13,27 @@ namespace _404_game_portal.backend.Controllers;
 public class AuthController(IAuthService authService) : ControllerBase
 {
     [HttpPost]
-    public async Task<ActionResult> Login([FromBody] string username, [FromBody] string password)
+    public async Task<ActionResult> Login([FromBody] LoginViewModel loginViewModel)
     {
-        if (!await authService.Authenticate(username, password))
+        if (!await authService.Authenticate(loginViewModel))
         {
             return BadRequest();
         }
 
+        var user = await authService.GetUser(loginViewModel.EmailOrUsername);
+
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.Username),
+        };
+
+        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var principal = new ClaimsPrincipal(identity);
+
         await HttpContext.SignInAsync(
             CookieAuthenticationDefaults.AuthenticationScheme,
-            new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, username) })),
-            new AuthenticationProperties { IsPersistent = true }
+            principal,
+            new AuthenticationProperties { IsPersistent = true}
         );
 
         return Ok();
@@ -34,10 +46,17 @@ public class AuthController(IAuthService authService) : ControllerBase
         return Ok();
     }
 
-    [HttpPost]
-    public async Task<ActionResult> Register([FromBody] string email, [FromBody] string username, [FromBody] string password)
+    [HttpGet("user")]
+    [Authorize]
+    public async Task<ActionResult<UserViewModel>> GetUser([FromQuery] string usernameOrEmail)
     {
-        await authService.Register(email, username, password);
+        return Ok(await authService.GetUser(usernameOrEmail));
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult> Register([FromBody] UserCreationViewModel userViewModel)
+    {
+        await authService.Register(userViewModel);
         return Ok();
     }
 }
--- 404_game_portal.backend/Services/AuthService.cs
+++ 404_game_portal.backend/Services/AuthService.cs
@@ -1,24 +1,32 @@
 using System.Security.Cryptography;
 using _404_game_portal.backend.Repositories;
+using _404_game_portal.backend.ViewModels;
 
 namespace _404_game_portal.backend.Services;
 
 public interface IAuthService
 {
-    public Task<bool> Authenticate(string emailOrUsername, string password);
-    public Task Register(string email, string username, string password);
+    public Task<bool> Authenticate(LoginViewModel loginViewModel);
+    public Task Register(UserCreationViewModel userViewModel);
+    Task<UserViewModel> GetUser(string usernameOrEmail);
 }
 
 public class AuthService(IUserRepository userRepository) : IAuthService
 {
-    public async Task<bool> Authenticate(string emailOrUsername, string password)
+    public async Task<bool> Authenticate(LoginViewModel loginViewModel)
     {
-        var user = await userRepository.GetByMailOrUsername(emailOrUsername);
-        return PasswordHasher.Verify(password, user.Password);
+        var user = await userRepository.GetByMailOrUsername(loginViewModel.EmailOrUsername);
+        return PasswordHasher.Verify(loginViewModel.Password, user.Password);
     }
 
-    public Task Register(string email, string username, string password)
+    public Task Register(UserCreationViewModel userViewModel)
     {
-        return userRepository.Register(email, username, PasswordHasher.Hash(password));
+        return userRepository.Register(userViewModel.Email, userViewModel.Username,
+            PasswordHasher.Hash(userViewModel.Password));
+    }
+
+    public async Task<UserViewModel> GetUser(string usernameOrEmail)
+    {
+        return new UserViewModel(await userRepository.GetByMailOrUsername(usernameOrEmail));
     }
 }
