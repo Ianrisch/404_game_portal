2d192a2f66c746f7409cda12023758315cf07644
Ian Risch <ian.risch@gmail.com>
2024-02-07 13:17:34 +0100

    404GP-3 Order All GameCards in a grid

--- 404_game_portal.frontend/src/views/Home.vue
+++ 404_game_portal.frontend/src/views/Home.vue
@@ -18,9 +18,9 @@ createPromise();
 <style lang="scss" scoped>
 .v-list {
   padding: 15px 20px 0 20px;
-
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: start;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 10px;
+  justify-items: center;
 }
 </style>
