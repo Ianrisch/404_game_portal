e85bc41be16b339930cb3d3391186e71d8df1868
Ian Risch <ian.risch@gmail.com>
2024-02-07 10:48:10 +0100

    Add Custom Authorise Attribute

---
+++ 404_game_portal.backend/Attributes/CustomAuthorizeAttribute.cs
@@ -0,0 +1,44 @@
+using System.Security.Claims;
+using _404_game_portal.backend.Enums;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+
+namespace _404_game_portal.backend.Attributes;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class CustomAuthorizeAttribute : Attribute, IAuthorizationFilter
+{
+    private readonly IList<Role> _roles;
+
+    public CustomAuthorizeAttribute(params Role[]? roles)
+    {
+        _roles = roles ?? [];
+    }
+
+    public async void OnAuthorization(AuthorizationFilterContext context)
+    {
+        // skip authorization if action is decorated with [AllowAnonymous] attribute
+        var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
+        if (allowAnonymous)
+            return;
+
+        // authorization
+        var roles = context.HttpContext.User.Claims
+            .Where(c => c.Type == ClaimTypes.Role)
+            .Select(c => (Role)Enum.Parse(typeof(Role), c.Value))
+            .ToList();
+
+        var isUnauthenticated =
+            !(await context.HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme)).Succeeded;
+
+        if (isUnauthenticated || (_roles.Any() && !roles.Any(r => _roles.Contains(r))))
+        {
+            context.Result = new JsonResult(new { message = "Unauthorized" })
+                { StatusCode = StatusCodes.Status401Unauthorized };
+        }
+    }
+}
