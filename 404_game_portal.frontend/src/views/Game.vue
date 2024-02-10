d0195772485934f5210e747623ff0d4d25a65151
ThunfischEis <ian.risch@gmail.com>
2024-02-10 17:39:39 +0100

    404GP-3 Refactor GameView

--- 404_game_portal.frontend/src/views/Game.vue
+++ 404_game_portal.frontend/src/views/Game.vue
@@ -19,24 +19,22 @@ watch(
 </script>
 
 <template>
-  <VCard v-if="result">
-    <VCardItem>
-      <v-btn icon @click="$router.go(-1)" variant="tonal">
-        <v-icon icon="mdi-arrow-left" />
-      </v-btn>
-    </VCardItem>
-    <VCardItem>
-      <img :src="result!.image ?? 'https://picsum.photos/1000/?random=' + result.id" alt="test" />
-    </VCardItem>
+  <v-card v-if="result">
+    <v-card-item>
+      <v-btn icon="mdi-arrow-left" to=".." variant="tonal" />
+    </v-card-item>
+    <v-card-item>
+      <img :src="result.image ?? 'https://picsum.photos/1000/?random=' + result.id" alt="test" />
+    </v-card-item>
     <div>
-      <VCardTitle>{{ result!.name }}</VCardTitle>
-      <VCardText>{{ result!.description }}</VCardText>
-      <VCardItem>
-        <div>{{ USK[result!.usk] }}</div>
-        <div>Release Date: {{ result!.releaseDate.toLocaleString() }}</div>
+      <v-card-title>{{ result.name }}</v-card-title>
+      <v-card-text>{{ result.description }}</v-card-text>
+      <v-card-item>
+        <div>{{ USK[result.usk] }}</div>
+        <div>Release Date: {{ result.releaseDate.toLocaleString() }}</div>
         <v-chip-group>
           <v-chip
-            v-for="platformAndPrice in result!.platformAndPrices"
+            v-for="platformAndPrice in result.platformAndPrices"
             :key="platformAndPrice.id"
             :ripple="false"
           >
@@ -45,7 +43,7 @@ watch(
           </v-chip>
         </v-chip-group>
         <v-chip-group>
-          <v-chip v-for="feature in result!.features" :key="feature.featureName" :ripple="false">
+          <v-chip v-for="feature in result.features" :key="feature.featureName" :ripple="false">
             {{ feature.featureName }}
             <v-tooltip activator="parent" location="top">
               {{ feature.featureDescription }}
@@ -53,13 +51,13 @@ watch(
           </v-chip>
         </v-chip-group>
         <v-chip-group>
-          <v-chip v-for="language in result!.languages" :key="language.id" :ripple="false">
+          <v-chip v-for="language in result.languages" :key="language.id" :ripple="false">
             {{ language.languageName }}
           </v-chip>
         </v-chip-group>
-      </VCardItem>
+      </v-card-item>
     </div>
-  </VCard>
+  </v-card>
 </template>
 
 <style scoped>
