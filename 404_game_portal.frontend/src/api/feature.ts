97e3891a13e8679d578ca8029f9259d188d5064b
ThunfischEis <ian.risch@gmail.com>
2024-02-12 15:57:47 +0100

    404GP-3 Add Feature Endpoints

--- 404_game_portal.frontend/src/api/feature.ts
+++ 404_game_portal.frontend/src/api/feature.ts
@@ -8,6 +8,14 @@ export type Feature = {
   featureDescription: string;
 };
 
+export type FeatureCreationData = {
+  gameIds: string[];
+  featureName: string;
+  featureDescription: string;
+};
+
 export const fetchFeatures = async (): Promise<Feature[]> => httpClient.get('/api/feature');
-export const fetchFeature = async (id: String): Promise<Feature> =>
+export const fetchFeature = async (id: string): Promise<Feature> =>
   httpClient.get('/api/feature/' + id);
+export const createFeature = async (data: FeatureCreationData): Promise<Feature> =>
+  httpClient.post('/api/feature', data);
