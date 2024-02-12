a05367fce406b45161e283dfd8ca30ddd1c7d126
ThunfischEis <ian.risch@gmail.com>
2024-02-12 15:58:06 +0100

    404GP-3 Add Game Endpoints

--- 404_game_portal.frontend/src/api/game.ts
+++ 404_game_portal.frontend/src/api/game.ts
@@ -2,6 +2,7 @@ import httpClient from '@/services/httpClient';
 import { Feature } from '@/api/feature';
 import { PlatformAndPrice } from '@/api/platformAndPrice';
 import { Language } from '@/api/language';
+import { da } from 'vuetify/locale';
 
 export enum USK {
   USK0,
@@ -23,5 +24,22 @@ export type Game = {
   languages: Language[];
 };
 
+export type priceAndPlatformId = {
+  platformId: string;
+  price: number;
+};
+
+export type GameCreationData = {
+  name: string;
+  platforms: priceAndPlatformId[];
+  usk: USK;
+  releaseDate: Date;
+  description: string;
+  features: string[];
+  languages: string[];
+};
+
 export const fetchGames = async (): Promise<Game[]> => httpClient.get('/api/game');
 export const fetchGame = async (id: string): Promise<Game> => httpClient.get(`/api/game/${id}`);
+export const createGame = async (data: GameCreationData): Promise<Game> =>
+  httpClient.post(`/api/game`, data);
