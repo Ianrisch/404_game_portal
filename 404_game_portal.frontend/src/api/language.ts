1e92dede5d215a77627659736cb941fd29d52ce6
ThunfischEis <ian.risch@gmail.com>
2024-02-12 15:58:18 +0100

    404GP-3 Add Language Endpoints

--- 404_game_portal.frontend/src/api/language.ts
+++ 404_game_portal.frontend/src/api/language.ts
@@ -1,7 +1,18 @@
 import { Game } from '@/api/game';
+import httpClient from '@/services/httpClient';
 
 export type Language = {
   id: string;
   languageName: string;
   games: Game[];
 };
+
+export type LanguageCreationData = {
+  languageName: string;
+  gameIds: string[];
+};
+export const fetchLanguage = async (id: string): Promise<Language> =>
+  httpClient.get(`/api/language/${id}`);
+export const fetchLanguages = async (): Promise<Language[]> => httpClient.get(`/api/language`);
+export const createLanguage = async (data: LanguageCreationData): Promise<Language> =>
+  httpClient.post(`/api/language`, data);
