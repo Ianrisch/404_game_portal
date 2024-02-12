72be477bfafa73e8e54a43e85f7b9eaa995c9dd6
ThunfischEis <ian.risch@gmail.com>
2024-02-12 15:57:35 +0100

    404GP-3 Add Platform Endpoints

--- 404_game_portal.frontend/src/api/platformAndPrice.ts
+++ 404_game_portal.frontend/src/api/platformAndPrice.ts
@@ -1,5 +1,5 @@
 import httpClient from '@/services/httpClient';
-import { Game } from '@/api/game';
+import { Game, GameCreationData } from '@/api/game';
 
 export type PlatformAndPrice = {
   id: string;
@@ -9,3 +9,29 @@ export type PlatformAndPrice = {
   platformVersion: string;
   platformType: string;
 };
+export type Platform = {
+  id: string;
+  price: number;
+  platformName: string;
+  platformVersion: string;
+  platformType: string;
+};
+
+export type GameAndPrice = {
+  gameId: string;
+  price: number;
+};
+
+export type PlatformCreationData = {
+  gamesAndPrices: GameAndPrice[];
+  platformName: string;
+  platformVersion: string;
+  platformType: string;
+};
+
+export const createPlatform = async (data: PlatformCreationData): Promise<Platform> =>
+  httpClient.post(`/api/platform`, data);
+
+export const fetchPlatform = async (id: string): Promise<Platform> =>
+  httpClient.get(`/api/platform/${id}`);
+export const fetchPlatforms = async (): Promise<Platform[]> => httpClient.get(`/api/platform}`);
