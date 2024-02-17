fa1fa092c4a9c802cd0c00684d7a18a7ff2803e3
jlange <jlange54@gmx.de>
2024-02-17 17:17:26 +0100

    404GP-4 Add Comment Viewmodels

---
+++ 404_game_portal.backend/ViewModels/CommentUpdateViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace _404_game_portal.backend.ViewModels;
+
+public class CommentUpdateViewModel
+{
+    public Guid Id { get; set; }
+    public string Comment { get; set; }
+}
---
+++ 404_game_portal.backend/ViewModels/CommentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using _404_game_portal.backend.Entities;
+
+namespace _404_game_portal.backend.ViewModels;
+
+public class CommentViewModel
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid GameId { get; set; }
+    public string Comment { get; set; }
+
+    public CommentViewModel(GameComment gameComment)
+    {
+        Id = gameComment.Id;
+        UserId = gameComment.UserId;
+        GameId = gameComment.GameId;
+        Comment = gameComment.Comment;
+    }
+}
