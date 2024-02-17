8f9b5b744f4f365d21fabb4a5645b022307bcd81
jlange <jlange54@gmx.de>
2024-02-17 17:18:00 +0100

    404GP-4 Add CommentCreation & Update Dto´s

---
+++ 404_game_portal.backend/Dto/CommentCreationDto.cs
@@ -0,0 +1,17 @@
+﻿using _404_game_portal.backend.Entities;
+using _404_game_portal.backend.ViewModels;
+
+namespace _404_game_portal.backend.Dto;
+
+public class CommentCreationDto
+{
+    public CommentCreationDto(CommentCreationViewModel creationViewModel)
+    {
+        Comment = creationViewModel.Comment;
+        GameId = creationViewModel.GameId;
+    }
+    
+    public Guid UserId { get; set; }
+    public Guid GameId { get; set; }
+    public string Comment { get; set; }
+}
---
+++ 404_game_portal.backend/Dto/CommentUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using _404_game_portal.backend.ViewModels;
+
+namespace _404_game_portal.backend.Dto;
+
+public class CommentUpdateDto
+{
+    public CommentUpdateDto(CommentUpdateViewModel updateViewModel)
+    {
+        Comment = updateViewModel.Comment;
+        Id = updateViewModel.Id;
+    }
+    public Guid UserId { get; set; }
+    public Guid Id { get; set; }
+    public string Comment { get; set; }
+}
