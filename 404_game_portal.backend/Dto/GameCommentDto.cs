1a69ade2e28cf0438b20f7f9b47e5dbef6d82f6b
jlange <jlange54@gmx.de>
2024-02-18 12:58:55 +0100

    404GP-4 Add GameCommentDto

---
+++ 404_game_portal.backend/Dto/GameCommentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace _404_game_portal.backend.Dto;
+
+public class GameCommentDto
+{
+    public Guid Id { get; set; }
+    public Guid GameId { get; set; }
+    public string Comment { get; set; }
+    public GameCommentUserDto UserDto { get; set; }
+}
